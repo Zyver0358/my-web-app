4c7f41447f0d69b6ebdb53ccd9176fc953401cd4
Zyver0358 <anonuevorudy0358@gmail.com>
2026-02-08 23:28:52 +0800

    Add service worker for asset caching
    Implement service worker for caching assets and offline support

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-self.addEventListener('fetch', function(event) {
-  // This is a dummy service worker to allow installation
+const CACHE_NAME = 'my-app-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+// Install: Save files to the phone's memory
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch: Serve files from memory if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
